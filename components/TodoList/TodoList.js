84fde99d96676a317b6ee2ac33f6c2050c49a407
Magap <guilhermeart2020@outlook.com>
2022-10-24 08:58:21 -0300

    components added

---
+++ components/TodoList/TodoList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './TodoList.module.css'
+
+const TodoList = () => {
+    return (
+        <h1 className={styles.h1}>Hi</h1>
+    )
+}
+
+export default TodoList
